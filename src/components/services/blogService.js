69d1221a6852ae919b1f77a24adc9fff3dcf837c
Mayquar <romain1.leclair@gmail.com>
2024-09-13 08:51:32 +0000

    Modification Initialisation Firebase

--- src/components/services/blogService.js
+++ src/components/services/blogService.js
@@ -1,6 +1,6 @@
-import { initializeApp } from "firebase/app";
+import { db } from '../../firebase'; // Assurez-vous que le chemin est correct
+
 import {
-  getFirestore,
   collection,
   addDoc,
   getDocs,
@@ -9,21 +9,6 @@ import {
   updateDoc,
   deleteDoc,
 } from "firebase/firestore";
-
-// Configuration Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyDZ4lsW9BpwhKO2cZg4kNEQNDqv5126NmU",
-  authDomain: "rdvp3-2a363.firebaseapp.com",
-  projectId: "rdvp3-2a363",
-  storageBucket: "rdvp3-2a363.appspot.com",
-  messagingSenderId: "681298416276",
-  appId: "1:681298416276:web:47df035976886e228c1a81",
-  measurementId: "G-4EQSTLHXC2",
-};
-
-// Initialisation de Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
 
 // Fonction pour ajouter un billet de blog
 export const addBlogPost = async (postData) => {
